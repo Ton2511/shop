04403d12161725f801506e1574737d7f98cd217e
Ton2511 <tonnum251139@gmail.com>
2025-04-30 00:41:09 +0700

    login token

--- db.js
+++ db.js
@@ -1,45 +1,11 @@
-// db.js - ไฟล์เชื่อมต่อฐานข้อมูล SQL
-const { Sequelize } = require('sequelize');
-
-// สร้าง Sequelize instance
-const sequelize = new Sequelize(
-  process.env.DB_NAME || 'shop_db', 
-  process.env.DB_USER || 'root', 
-  process.env.DB_PASSWORD || '', 
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
-    logging: console.log,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  }
-);
-
-// ฟังก์ชันสำหรับทดสอบการเชื่อมต่อ
-const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("✅ SQL Database Connected...");
-  } catch (err) {
-    console.error("❌ SQL Database Connection Error:", err);
-    process.exit(1);
-  }
-};
-
-module.exports = { sequelize, connectDB };
-
 // // db.js - ไฟล์เชื่อมต่อฐานข้อมูล SQL
 // const { Sequelize } = require('sequelize');
 
 // // สร้าง Sequelize instance
 // const sequelize = new Sequelize(
-//   process.env.DB_NAME || 'rpcint_nodejs', 
-//   process.env.DB_USER || 'rpcint_admin', 
-//   process.env.DB_PASSWORD || 'Arinkkn@517', 
+//   process.env.DB_NAME || 'shop_db', 
+//   process.env.DB_USER || 'root', 
+//   process.env.DB_PASSWORD || '', 
 //   {
 //     host: process.env.DB_HOST || 'localhost',
 //     dialect: 'mysql',
@@ -65,3 +31,37 @@ module.exports = { sequelize, connectDB };
 // };
 
 // module.exports = { sequelize, connectDB };
+
+// db.js - ไฟล์เชื่อมต่อฐานข้อมูล SQL
+const { Sequelize } = require('sequelize');
+
+// สร้าง Sequelize instance
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'rpcint_nodejs', 
+  process.env.DB_USER || 'rpcint_admin', 
+  process.env.DB_PASSWORD || 'Arinkkn@517', 
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+    logging: console.log,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+);
+
+// ฟังก์ชันสำหรับทดสอบการเชื่อมต่อ
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ SQL Database Connected...");
+  } catch (err) {
+    console.error("❌ SQL Database Connection Error:", err);
+    process.exit(1);
+  }
+};
+
+module.exports = { sequelize, connectDB };
